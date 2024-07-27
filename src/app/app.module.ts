5478660fae6ed0ea95f74d8d3120a14e9be265c7
Wendell Delmondes <wendell2009@hotmail.com>
2024-07-27 19:12:49 -0300

    1 angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatButtonModule} from '@angular/material/button';//acrescentado
+import {MatIconModule} from '@angular/material/icon';//acrescentado
+import {MatDividerModule} from '@angular/material/divider';//acrescentado
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,//acrescentado
+    MatIconModule,//acrescentado
+    MatDividerModule//acrescentado
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
