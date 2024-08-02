83d5e4f15847873579aeb02377c219d8f81afe69
Wendell Delmondes <wendell2009@hotmail.com>
2024-08-02 13:04:23 -0300

    concluido com logica de filtro no app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { USERS_LIST_DATA } from './data/user-list';
 import { IFilterOptions } from './interfaces/user/filter-options.interface';
-
+import { isWithinInterval } from "date-fns";
 
 @Component({
   selector: 'app-root',
@@ -36,6 +36,8 @@ export class AppComponent implements OnInit {
   filterUsersList(filterOptions: IFilterOptions,usersList:IUser[]): IUser[] {
     let filteredList: IUser[] = [] ;
     filteredList=this.filterListByName(filterOptions.name,usersList);
+    filteredList=this.filterListByStatus(filterOptions.status,filteredList);
+    filteredList=this.filterListByDate(filterOptions.starDate,filterOptions.endDate,filteredList);
     return filteredList;
   }
   //////////////////////////
@@ -51,10 +53,35 @@ export class AppComponent implements OnInit {
     
     return filteredListByName;
   }
-  filterListByStatus(status: boolean, filteredList: IUser) {
-    return filteredList;
+  filterListByStatus(status: boolean | undefined, filteredList: IUser[]) {
+    const STATUS_NOT_SELECTED = status===undefined;
+    if(STATUS_NOT_SELECTED){
+      return filteredList;
+    }
+    const filteredListByStatus=filteredList.filter((user)=>user.ativo===status);
+    return filteredListByStatus;
   }
-  filterListByDate(date: Date, filteredList: IUser) {
+  filterListByDate(starDate: Date | undefined,endDate:Date | undefined , filteredList: IUser[]) { //adicionado pacote npm i date-fns(dependência de produção)
+    const DATES_NOT_SELECTED = starDate===undefined || endDate===undefined; // não usa o --save-dev, ver package.json em dependecias
+    if(DATES_NOT_SELECTED){
+      return filteredList;
+    }
+    const filteredListByDate=filteredList.filter((user)=>isWithinInterval(user.dataCadastro,{
+      start:starDate,
+      end:endDate
+    }));
+    return filteredListByDate;
+    ///OU
+    // const checkDateInterval = (user:IUser) => isWithinInterval(new Date(user.dataCadastro),{
+    //   star:starDate,
+    //   end:endDate
+    // });
+    // const filteredListByDate =filteredList.filter(checkDateInterval);
+  }
+
+
+  filterListByDate2(starDate: Date | undefined,endDate:Date | undefined , filteredList: IUser[]) {
+    
     return filteredList;
   }
   /////////////////////////////
